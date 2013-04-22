1d15d0baff7d5add8a9c4b223f619c1f04111b27
Emma Strubell <strubell@cs.umass.edu>
2013-04-22 19:17:35 -0400

    more changes to vary_constants.py

--- vary_constants.py
+++ vary_constants.py
@@ -12,17 +12,28 @@ data_fname = "bin/output"
 programName = "./bin/VPNNet-exp"
 
 setSize = 10
-initialPrecision = 4
-trainingIters = 200
-interval = 1.0
-min_k1 = 0.0
-max_k1 = 10.0
-min_k2 = min_k1
+initialPrecision = 10
+trainingIters = 125
+min_k1 = 1.0
+max_k1 = 0.0
+min_k2 = 1.0 #min_k1
 max_k2 = max_k1
-eta = 1.0
+interval = 1.0
+eta = 5.0
 
-num_k1_vals = (min_k1+max_k1)/interval
-num_k2_vals = (min_k2+max_k2)/interval
+vary_precision = True
+vary_constants = False
+
+if not vary_constants: 
+	if vary_precision:
+		# varying precision but only with one set of constants
+		interval = 2.0
+	else:
+		# not messing with precision at all
+		interval = 1.0
+	
+num_k1_vals = (min_k1+max_k1+1.0)/interval
+num_k2_vals = (min_k2+max_k2+1.0)/interval
 
 k1_vals = linspace(min_k1, max_k1, num_k1_vals)
 k2_vals = linspace(min_k2, max_k2, num_k2_vals)
@@ -69,12 +80,15 @@ for i in range(int(num_k1_vals)):
 #ax.set_yscale("log")
 ax.set_ylabel("Training Error")
 ax.set_xlabel("Iteration")
-ax.set_title("$\Delta$ Training Error, Precision over Time ($s$ = %d, $\eta$ = %f)" % (setSize, eta))
+ax.set_title("$\Delta$ Training Error, Precision over Time ($s$ = %d, $\eta$ = %g, $p$ = %g)" % (setSize, eta, initialPrecision))
 #ax.set_ylim([amin(errors), amax(errors)])
 ax.set_ylim([-1.0, 1.0])
 ax.set_xlim([0.0,trainingIters])
-cb = plt.colorbar(lc)
-cb.set_label("Precision (bits)")
+
+# don't bother making color bar if not changing precision
+if vary_precision:
+	cb = plt.colorbar(lc)
+	cb.set_label("Precision (bits)")
 
 #plt.legend(["min = %f" % (r_min1), "min = %f" % (r_min2), "min = %f" % (r_min3)], shadow=True)
 #plt.xlim((r_low,r_high-r_int))
@@ -82,5 +96,6 @@ cb.set_label("Precision (bits)")
 saveFormat = "pdf"
 saveName = "data/error-prec-vs-time-ss" + str(setSize) + "-iters" + str(trainingIters) + "-eta" + str(eta) +  "-prec" + str(initialPrecision) + "." + saveFormat
 plt.savefig(saveName)
+print "Saved plot to file: %s" % (saveName)
 plt.show()
 
